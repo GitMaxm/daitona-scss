ce6935b0ceac156507afae934fd380823d688df6
Maxim <115576894+GitMaxm@users.noreply.github.com>
2024-06-01 14:39:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,48 @@
 function btnDarkMode() {
-
     const btnDarkMode = document.querySelector(".dark-mode-btn");
+    const btnIconMode = document.querySelector(".dark-mode-btn__icon");
 
     // 1. Проверка темной темы на уровне системных настроек
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
         btnDarkMode.classList.add("dark-mode-btn--active");
         document.body.classList.add("dark");
+        btnIconMode.src = './img/icons/moon.svg'; // обновляем иконку
+        btnIconMode.style.left = "26px"; // устанавливаем позицию
     }
 
     // 2. Проверка темной темы в localStorage
     if (localStorage.getItem('darkMode') === 'dark') {
         btnDarkMode.classList.add("dark-mode-btn--active");
         document.body.classList.add("dark");
+        btnIconMode.src = './img/icons/moon.svg'; // обновляем иконку
+        btnIconMode.style.left = "26px"; // устанавливаем позицию
+
     } else if (localStorage.getItem("darkMode") === "light") {
         btnDarkMode.classList.remove("dark-mode-btn--active");
         document.body.classList.remove("dark");
+        btnIconMode.src = './img/icons/sun.svg'; // обновляем иконку
+        btnIconMode.style.left = "0"; // сбрасываем позицию
     }
 
     // Если меняются системные настройки, меняем тему
-    window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .addEventListener("change", (event) => {
-            const newColorScheme = event.matches ? "dark" : "light";
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
 
-            if (newColorScheme === "dark") {
-                btnDarkMode.classList.add("dark-mode-btn--active");
-                document.body.classList.add("dark");
-                localStorage.setItem("darkMode", "dark");
-            } else {
-                btnDarkMode.classList.remove("dark-mode-btn--active");
-                document.body.classList.remove("dark");
-                localStorage.setItem("darkMode", "light");
-            }
-        });
+        if (newColorScheme === "dark") {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark");
+            btnIconMode.src = './img/icons/moon.svg'; // обновляем иконку
+            btnIconMode.style.left = "26px"; // устанавливаем позицию
+            localStorage.setItem("darkMode", "dark");
+
+        } else {
+            btnDarkMode.classList.remove("dark-mode-btn--active");
+            document.body.classList.remove("dark");
+            btnIconMode.src = './img/icons/sun.svg'; // обновляем иконку
+            btnIconMode.style.left = "0"; // сбрасываем позицию
+            localStorage.setItem("darkMode", "light");
+        }
+    });
 
     // Включение ночного режима по кнопке
     btnDarkMode.onclick = function () {
@@ -40,11 +50,16 @@ function btnDarkMode() {
         const isDark = document.body.classList.toggle("dark");
 
         if (isDark) {
+            btnIconMode.src = './img/icons/moon.svg'; // обновляем иконку
+            btnIconMode.style.left = "26px"; // устанавливаем позицию
             localStorage.setItem("darkMode", "dark");
         } else {
+            btnIconMode.src = './img/icons/sun.svg'; // обновляем иконку
+            btnIconMode.style.left = "0"; // сбрасываем позицию
             localStorage.setItem("darkMode", "light");
         }
     };
-};
+}
 
-btnDarkMode();
+// Убедимся, что DOM загружен, прежде чем выполнять функцию
+document.addEventListener('DOMContentLoaded', btnDarkMode);
