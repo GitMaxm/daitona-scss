b3d861f2ca1efdc48ff8c77dbc5398802da58f17
Maxim <115576894+GitMaxm@users.noreply.github.com>
2024-11-11 18:16:27 +0300

    Add files via upload

---
+++ js/services.js
@@ -0,0 +1,49 @@
+const servicesSwiper = new Swiper('.services-swiper', {
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	speed: 1000,
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 30,
+
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+
+	autoplay: {
+		delay: 2000,
+		disableOnInteraction: true,
+	},
+
+	keyboard: {
+		enabled: true,
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: true,
+	},
+
+	// Адаптивные настройки для различных разрешений
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		600: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+
+		970: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	},
+});
